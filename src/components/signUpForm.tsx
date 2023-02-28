32f2eef091ce8c27a406c61bdf3cfb1f63077242
bkaygisiz <b.kaygisiz@live.fr>
2023-02-28 20:08:18 +0100

    rresponsive done on navigation bar and signup page

--- src/components/signUpForm.tsx
+++ src/components/signUpForm.tsx
@@ -83,72 +83,70 @@ export default function SignUpForm() {
 
     return (
         <>
-            <main className="bg-gradient-to-r from-gray-900 to-slate-800 h-screen flex justify-center items-center text-slate-300">
-                <form className="flex flex-col w-2/5 py-10 h-auto justify-center items-center animate-fadin shadow-slate-900 shadow-2xl rounded-lg ring-2 ring-slate-800 bg-gradient-to-r from-slate-800 to-gray-900 space-y-8">
-                    <h1 className="text-slate-100 text-5xl font-bold break-words">Sign up</h1>
-                    <div className='w-4/6 flex flex-col items-center'>
-                        <label className='block font-bold w-4/5 break-words'>
-                            <HiOutlineMail className='text-xl inline-block' /> Email
-                        </label>
-                        <input
-                            type="text"
-                            name='email'
-                            onChange={handleChange}
-                            placeholder='Email'
-                            className="bg-slate-900 w-4/5 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
-                        />
-                        <p className="text-red-500 w-4/5 mt-1">{errorEmail}</p>
-                    </div>
-                    <div className='w-4/6 flex flex-col items-center'>
-                        <label className='block font-bold w-4/5 break-words'>
-                            <HiUserCircle className='inline-block text-xl' /> Username
-                        </label>
-                        <input
-                            type="text"
-                            name='username'
-                            onChange={handleChange}
-                            placeholder='Username'
-                            className="bg-slate-900 w-4/5 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
-                        />
-                        <p className="text-red-500 w-4/5 mt-1">{errorUsername}</p>
-                    </div>
-                    <div className='w-4/6 flex flex-col items-center'>
-                        <label className='block font-bold w-4/5 break-words'>
-                            <MdPassword className='inline-block text-xl' /> Password
-                        </label>
-                        <input
-                            type="password"
-                            name='password'
-                            onChange={handleChange}
-                            placeholder='************'
-                            className="bg-slate-900 w-4/5 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
-                        />
-                        <p className="text-red-500 break-all text-sm w-4/5 mt-1">{errorPassword}</p>
-                        <p className="text-slate-500 text-xs w-4/5 mt-1">Password security : {message}</p>
-                    </div>
-                    <div className='w-4/6 flex flex-col items-center'>
-                        <label className='block font-bold w-4/5 break-words'>
-                            <MdPassword className='inline-block text-xl' /> Comfirm password
-                        </label>
-                        <input
-                            type="password"
-                            name='comfirmPassword'
-                            onChange={handleChange}
-                            placeholder='************'
-                            className="bg-slate-900 w-4/5 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
-                        />
-                        <p className="text-red-500 w-4/5 mt-1">{errorConfirmPassword}</p>
-                    </div>
-                    <div className='w-4/6 flex flex-col items-center'>
-                        <button
-                            onClick={signUp}
-                            className="bg-slate-900 w-1/5 break-words p-1 rounded border-solid border border-slate-700 shadow-md hover:bg-slate-800 duration-300 active:bg-slate-900 active:ring-2 active:ring-green-700 focus:outline-none"
-                        >
-                           <HiCheckCircle className='inline-block text-xl'/> Sign up
-                        </button>
-                    </div>
-                </form>
-            </main>
+            <form className="flex flex-col lg:w-2/5 md:w-4/5 md:h-4/5 w-4/5 py-10 justify-center items-center animate-fadin shadow-slate-900 shadow-2xl rounded-2xl ring-2 ring-slate-800 bg-gradient-to-r from-slate-800 to-slate-900 lg:space-y-10 md:space-y-10 space-y-6">
+                <h1 className="text-slate-100 md:text-5xl text-4xl font-bold break-words">Sign up</h1>
+                <div className='w-4/6 flex flex-col items-center'>
+                    <label className='block font-bold w-4/5 break-words'>
+                        <HiOutlineMail className='text-xl inline-block' /> Email
+                    </label>
+                    <input
+                        type="text"
+                        name='email'
+                        onChange={handleChange}
+                        placeholder='Email'
+                        className="bg-slate-900 md:w-4/5 w-5/6 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
+                    />
+                    <p className="text-red-500 w-4/5 mt-1">{errorEmail}</p>
+                </div>
+                <div className='w-4/6 flex flex-col items-center'>
+                    <label className='block font-bold w-4/5 break-words'>
+                        <HiUserCircle className='inline-block text-xl' /> Username
+                    </label>
+                    <input
+                        type="text"
+                        name='username'
+                        onChange={handleChange}
+                        placeholder='Username'
+                        className="bg-slate-900 md:w-4/5 w-5/6 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
+                    />
+                    <p className="text-red-500 w-4/5 mt-1">{errorUsername}</p>
+                </div>
+                <div className='w-4/6 flex flex-col items-center'>
+                    <label className='block font-bold w-4/5 break-words'>
+                        <MdPassword className='inline-block text-xl' /> Password
+                    </label>
+                    <input
+                        type="password"
+                        name='password'
+                        onChange={handleChange}
+                        placeholder='************'
+                        className="bg-slate-900 md:w-4/5 w-5/6 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
+                    />
+                    <p className="text-red-500 break-all text-sm w-4/5 mt-1">{errorPassword}</p>
+                    <p className="text-slate-500 text-xs w-4/5 mt-1">Password security : {message}</p>
+                </div>
+                <div className='w-4/6 flex flex-col items-center'>
+                    <label className='block font-bold w-4/5 break-words'>
+                        <MdPassword className='inline-block text-xl' /> Comfirm password
+                    </label>
+                    <input
+                        type="password"
+                        name='comfirmPassword'
+                        onChange={handleChange}
+                        placeholder='************'
+                        className="bg-slate-900 md:w-4/5 w-5/6 p-1 ring-1 ring-slate-800 rounded shadow-md hover:bg-slate-800 duration-300 focus:outline-none"
+                    />
+                    <p className="text-red-500 w-4/5 mt-1">{errorConfirmPassword}</p>
+                </div>
+                <div className='w-4/6 flex flex-col items-center'>
+                    <button
+                        onClick={signUp}
+                        className="bg-slate-900 lg:w-1/5 md:w-28 w-3/5 break-words p-1 rounded-sm border-solid border border-slate-700 shadow-md hover:bg-slate-800 duration-300 active:bg-slate-900 active:ring-2 active:ring-green-700 focus:outline-none"
+                    >
+                        <HiCheckCircle className='inline-block text-xl' /> Sign up
+                    </button>
+                </div>
+            </form>
         </>
     );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -6,7 +6,7 @@ export default function SignUp() {
     return (
         <>
             <Navigation />
-            <main>
+            <main className='bg-gradient-to-r from-indigo-900 to-slate-900 h-screen flex justify-center items-center text-slate-300'>
                 <SignUpForm/>
             </main>
         </>
